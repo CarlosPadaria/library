d53b530b2a6760ec3582e2da07a034eed0a24a50
Felipe José Malicheski Januário <felipemalicheski@gmail.com>
2024-03-10 16:09:44 -0300

    fix: arrumar conflitos do Doctrine ORM
    Anteriormente não era possível usar os comandos do orm:schema-tool. Este problema foi corrigido.

---
+++ server/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+
+// bootstrap.php
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once 'vendor/autoload.php';
+
+// Create a simple "default" Doctrine ORM configuration for Attributes
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: [__DIR__ . '/Library/Model/Entity'],
+    isDevMode: true,
+);
+
+// configuring the database connection
+$connection = DriverManager::getConnection([
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'library',
+    'user' => 'root',
+    'password' => '',
+], $config);
+
+// obtaining the entity manager
+$entityManager = new EntityManager($connection, $config);
+
+/**
+ * Define o entityManager como constante a ser utilizada em toda a aplicação
+ */
+if (!defined('em')) {
+    define('em', $entityManager);
+}
